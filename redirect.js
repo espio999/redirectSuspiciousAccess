0732c1f821c46fcfeb4fc504c4dfbc60e509c411
espio999 <espio999@users.noreply.github.com>
2026-01-27 13:02:15 +0900

    Add files via upload

--- redirect.js
+++ redirect.js
@@ -1,6 +1,6 @@
 async function executeRedirect(source){
     const CONFIG = {
-        REDIRECT_BASE: "https://127.0.0.1",
+        REDIRECT_BASE: "https://0.0.0.0",
         //WARNING_URL: "https://impsbl.hatenablog.jp/entry/20251231",
         //WARNING_HASH: "#%E4%B8%8D%E5%AF%A9%E3%82%A2%E3%82%AF%E3%82%BB%E3%82%B9%E3%81%AEredirect%E5%85%88Redirect-Destination-for-Suspicious-Access"
     };
