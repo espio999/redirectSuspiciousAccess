d05ad2e2db8f2604ab6450640730bc41ca5fea1e
espio999 <espio999@users.noreply.github.com>
2026-02-28 14:31:27 +0900

    Add files via upload

--- redirect.js
+++ redirect.js
@@ -1,7 +1,7 @@
 function executeRedirect(){
     const CONFIG = {
       REDIRECT_BASE: "https://0.0.0.0",
-      WARNING_URL: "https://impsbl.hatenablog.jp/entry/20260129",
+      WARNING_URL: "https://impsbl.hatenablog.jp/entry/20260228",
       WARNING_HASH: "#Redirect-Destination-for-Suspicious-Access%E4%B8%8D%E5%AF%A9%E3%82%A2%E3%82%AF%E3%82%BB%E3%82%B9%E3%81%AE%E8%BB%A2%E9%80%81%E5%85%88",
       // 本来のページURLをパラメータとして付与
       ORIGINAL_URL: `?from=${encodeURIComponent(window.location.href)}`
